fa845f32c6ad50b8c3b62ee67cfb3422276cda31
Bananaslama277 <tracesmith555@gmail.com>
2023-05-05 08:21:21 -0500

    Initial Commit

---
+++ traceCapstone2/traceCapstone2App.swift
@@ -0,0 +1,17 @@
+//
+//  traceCapstone2App.swift
+//  traceCapstone2
+//
+//  Created by Trace Smith on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct traceCapstone2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
